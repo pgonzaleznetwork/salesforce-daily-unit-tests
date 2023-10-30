e6ea695d273ae72e4713f9bf7f2bbfeb3a2fff48
Pablo Gonzalez <pgonzaleznetwork@gmail.com>
2023-10-30 23:13:38 +0000

    create payload with js

--- parseResult.js
+++ parseResult.js
@@ -2,8 +2,41 @@ const fs = require('fs')
 
 let testResults = JSON.parse(fs.readFileSync('result.json'));
 let summary = testResults.result.summary;
+let tests = testResults.result.tests;
 
+let slackPayload = {
+    text: 'Integration Test Runs Finished',
+    blocks: []
+}
 
-let friendlySummary = `Automated unit testing for ${summary.username} (${summary.hostname}) has ${summary.outcome} with ${summary.testsRan} test runs and ${summary.failing} failure(s)`;
+let summaryBlock = {
+    type: 'section',
+    text: {
+        type: 'mrkdwn',
+        text: `Automated unit testing for ${summary.username} (${summary.hostname}) has ${summary.outcome} with ${summary.testsRan} test runs and ${summary.failing} failure(s)`
+    }
+}
 
-console.log(friendlySummary)
+slackPayload.blocks.push(summaryBlock);
+
+for(const test of tests){
+
+    if(test.Outcome == 'Fail'){
+        
+        let block = {
+            type: 'section',
+            text: {
+                type: 'mrkdwn',
+                text: `${test.FullName}: ${test.Message}`
+            }
+        }
+
+        slackPayload.blocks.push(block);
+    }
+}
+
+// Convert the object to a JSON string
+const jsonData = JSON.stringify(slackPayload); 
+
+// Write the JSON string to the file
+fs.writeFileSync('slackPayload.json', jsonData, 'utf-8');
