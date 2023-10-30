c849718d2139ecb212243a77065b7b734a7fcd54
Pablo Gonzalez <pgonzaleznetwork@gmail.com>
2023-10-30 15:35:18 +0000

    brief message

--- parseResult.js
+++ parseResult.js
@@ -2,27 +2,8 @@ const fs = require('fs')
 
 let testResults = JSON.parse(fs.readFileSync('result.json'));
 let summary = testResults.result.summary;
-let tests = testResults.result.tests;
 
 
-let friendlySummary = `Automated unit testing for ${summary.username} (${summary.hostname}) has ${summary.outcome} with ${summary.testsRan} and ${summary.failing} failures`;
+let friendlySummary = `Automated unit testing for ${summary.username} (${summary.hostname}) has ${summary.outcome} with ${summary.testsRan} test runs and ${summary.failing} failure(s)`;
 
-let failedTests = '';
-let containsFailures = false;
-
-for(const test of tests){
-
-    if(test.Outcome == 'Fail'){
-        
-        containsFailures = true;
-
-        failedTests += `${test.FullName}\n`
-        failedTests += `${test.Message}\n`
-        failedTests += `${test.StackTrace}\n`
-        failedTests += `\n`
-    }
-}
-
-let wholeMessage = `${friendlySummary}\n\n${failedTests}`
-
-console.log(wholeMessage)
+console.log(friendlySummary)
