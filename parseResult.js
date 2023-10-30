1713545fe301e347ca786342a37e99e981dc0533
Pablo Gonzalez <pgonzaleznetwork@gmail.com>
2023-10-30 14:45:42 +0000

    parse result and send with slack

---
+++ parseResult.js
@@ -0,0 +1,38 @@
+const fs = require('fs')
+
+let testResults = JSON.parse(fs.readFileSync('result.json'));
+let summary = testResults.result.summary;
+let tests = testResults.result.tests;
+
+
+let friendlySummary = `
+
+Completed test runs for ${summary.username} (${summary.hostname})
+
+Result: ${summary.outcome}
+Tests ran: ${summary.testsRan}
+Failed tests: ${summary.failing}
+
+`
+let failedTests = '';
+let containsFailures = false;
+
+for(const test of tests){
+
+    if(test.Outcome == 'Fail'){
+        
+        containsFailures = true;
+
+        failedTests += `${test.FullName}\n`
+        failedTests += `${test.Message}\n`
+        failedTests += `${test.StackTrace}\n`
+        failedTests += `\n`
+    }
+}
+
+let wholeMessage = `
+${friendlySummary}
+${failedTests}
+`
+
+console.log(wholeMessage)
